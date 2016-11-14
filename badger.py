6669e2bf4bc5bf4292dfbb8a877b4682551b6eab
Karl Menzel <menzelk@reed.edu>
2016-11-14 14:10:47 -0800

    Added inital graphspace functions and posted inital graph

--- badger.py
+++ badger.py
@@ -1,8 +1,41 @@
-import graphspace_utils, json_utils, gen_utils
+import graphspace_utils, json_utils
 
+
+
+def getEdgeAttributes(edges):
+    attrs = {} ## dictionary to return
+    #weights = normalize(weights)
+    print weights
+    for i in range(len(edges)):
+        e = edges[i]
+        source = e[0] ## set source node
+        target = e[1] ## set target node
+        if source not in attrs: ## initialize empty dict for source
+            attrs[source] = {} ## (only if not seen yet)
+        attrs[source][target] = {} ## init. (source,target) pair dict
+    return attrs
+
+def getNodeAttributes(nodes):
+    attrs = {} ## dictionary to return
+    for i in range(len(nodes)):
+        n = nodes[i]
+        attrs[n] = {} ## initialize empty dictionary for node n
+        attrs[n]['id'] = n ## set the id (REQUIRED)
+    return attrs
+
+def graphit(name, nodes, edges):
+    #nodeFile = getNodeAttributes(nodes)
+    nodeFile = None
+    #edgeFile = getEdgeAttributes(edges)
+    edgeFile = None
+    data = json_utils.make_json_data(nodes, edges, nodeFile, edgeFile, name, 'Desc.', [''])
+    jsonName = name + '.json'
+    json_utils.write_json(data, jsonName)
+    graphspace_utils.postGraph(name, jsonName, 'menzelk@reed.edu', 'sirstoat')
+    #graphspace_utils.shareGraph(name,'menzelk@reed.edu','sirstoat','Lab7','aritz@reed.edu')
 
 def readIn():
-    File = open('BadgerMartix.txt')
+    File = open('BadgerMatrix.txt')
     matrix = File.read()
     File.close()
     File = open('BadgerInfo.txt')
@@ -16,17 +49,43 @@ def readIn():
     badgerDemo = {}
     for i in info:
         row = i.split('\t')
-        badgerDemo[row[0]] = {head[1]:row[1], head[2]:row[2]}
+        badgerDemo[row[0]] = {head[1]:row[1], head[2]:row[2], head[3]:row[3]}
 
-    matrix = matrix.split('\t')
+    matrix = matrix.split('\n')
     badgers = matrix.pop(0)
-    badger = badgers.split('\t')
+    badgers = badgers.split('\t')
     badgers.pop(0)
     network = {}
     for i in range(len(matrix)):
         row = matrix[i].split('\t')
         badger = row.pop(0)
+        network[badger] = {}
         for j in range(len(badgers)):
-            network[badger][badgers[j]] = row[j]
-
+            row[j] = int(row[j])
+            if row[j] > 0:
+                network[badger][badgers[j]] = row[j]
     return badgerDemo, network
+
+def getEdgesList(dic):
+    edges = []
+    for i in dic:
+        for j in dic[i]:
+            if not([i,j] in edges) and not([j,i] in edges):
+                edges.append([i,j])
+    return edges
+
+def main():
+    badgers, matrix = readIn()
+    for i in badgers:
+        print i, badgers[i]
+    print matrix['008p']['012b']
+    edges = getEdgesList(matrix)
+    graphit('badgers_Test', badgers.keys(), edges)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
