e02fec4fd62de73f40d15d1c043121b67c62fb03
Karl Menzel <menzelk@reed.edu>
2016-11-14 13:25:06 -0800

    started code: readIn fucntion

---
+++ badger.py
@@ -0,0 +1,34 @@
+
+
+
+def readIn():
+    File = open('BadgerMartix.txt')
+    matrix = File.read()
+    File.close()
+    File = open('BadgerInfo.txt')
+    info = File.read()
+    File.close()
+
+
+    info = info.split('\n')
+    head = info.pop(0)
+    head = head.split('\t')
+    info.pop()
+    badgerDemo = {}
+    for i in info:
+        row = i.split('\t')
+        badgerDemo[row[0]] = {head[1]:row[1], head[2]:row[2]}
+
+    matrix = matrix.split('\t')
+    badgers = matrix.pop(0)
+    badger = badgers.split('\t')
+    badgers.pop(0)
+    network = {}
+    for i in range(len(matrix)):
+        row = matrix[i].split('\t')
+        badger = row.pop(0)
+        for j in range(len(badgers)):
+            network[badger][badgers[j]] = row[j]
+
+    return badgerDemo, network
+        
